d2ae1be8f1781e24b43aedaa5853097cdbe59186
gs-Hani <hani_alazawi@hotmail.com>
2024-05-01 07:58:36 +0400

    add transferMoney function

--- bank-system.c
+++ bank-system.c
@@ -16,6 +16,7 @@ void loginsu(void);
 void display(char*);
 // Level 3
 void checkBalance(char*);
+void transferMoney(void);
 // Level 4
 void initTermios(int echo);
 void resetTermios(void);
@@ -375,6 +376,13 @@ void display(char username1[]) {
     		case 1:
         		checkBalance(username1);
         		break;
+		case 2:
+			transferMoney();
+			break;
+		//case 3:
+		//	logout();
+		//	login();
+		//	break;
 
     		case 4:
         		exit(0);
@@ -443,6 +451,85 @@ void checkBalance(char username2[]) {
 	// Closing file after reading it
     	fclose(fm);
     	display(username2);
+}
+
+void transferMoney(void)
+{
+	int i,j;
+	FILE *fm, *fp;
+	struct pass u1;
+	struct money m1;
+	char usernamet[20];
+	char usernamep[20];
+	
+	system("clear");
+
+	// Opening file in read mode to read user's username
+	fp = fopen("username.txt", "rb");
+
+	// Creating a another file to write amount along with username to which amount is going to be transferred
+    	fm = fopen("mon.txt", "ab");
+
+	gotoxy(33, 4);
+    	printf("---- TRANSFER MONEY ----");
+    	gotoxy(33, 5);
+    	printf("========================");
+
+    	gotoxy(33, 11);
+    	printf("FROM (your username).. ");
+    	scanf("%s", &usernamet);
+
+    	gotoxy(33, 13);
+    	printf(" TO (username of person)..");
+    	scanf("%s", &usernamep);
+
+	// Checking for username if it is present in file or not
+	while (fread(&u1, sizeof(u1),1, fp)) {
+        	if (strcmp(usernamep,u1.username) == 0) {
+            		strcpy(m1.usernameto,u1.username);
+			strcpy(m1.userpersonfrom,usernamet);
+        	}
+    	}
+
+	gotoxy(33, 16);
+	// Taking amount input
+    	printf("ENTER THE AMOUNT TO BE TRANSFERRED..");
+    	scanf("%d", &m1.money1);
+
+	// Writing to the file
+   	fwrite(&m1, sizeof(m1),1, fm);
+
+	gotoxy(0, 26);
+    	printf(	"--------------------------------------------------"
+        	"--------------------------------------------");
+	gotoxy(0, 28);
+    	printf(	"--------------------------------------------------"
+        	"--------------------------------------------");
+	gotoxy(0, 29);
+    	printf("transferring amount, Please wait..");
+
+	gotoxy(10, 27);
+	for (i = 0; i < 70; i++) {
+        	for (j = 0; j < 1200000; j++) {
+            		j++;
+            		j--;
+        	}
+        	printf("*");
+    	}
+
+	gotoxy(33, 40);
+    	printf("AMOUNT SUCCESSFULLY TRANSFERRED....");
+	initTermios(0);	
+	getchar();
+	resetTermios();		
+
+	// Close the files
+    	fclose(fp);
+    	fclose(fm);
+
+	// Function to return to the home screen
+    	display(usernamet);
+
 }
 
 //*******************
